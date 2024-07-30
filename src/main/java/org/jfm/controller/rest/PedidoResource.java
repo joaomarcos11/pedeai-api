6e522f28a904afa1c6822582cc1fdd0ce83c182b
Murilo Martins <murilomartins@outlook.com>
2024-07-30 20:04:12 -0300

    Adicionadas anotações nos controllers

--- src/main/java/org/jfm/controller/rest/PedidoResource.java
+++ src/main/java/org/jfm/controller/rest/PedidoResource.java
@@ -5,6 +5,14 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jfm.controller.rest.dto.PedidoCreateDto;
 import org.jfm.controller.rest.dto.PedidoDto;
@@ -42,15 +50,39 @@ public class PedidoResource {
     @Inject
     PedidoMapper pedidoMapper;
 
+    @Operation(summary = "Criar pedido", description = "Cria um novo pedido")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "201", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Pagamento.class)) }),
+            @APIResponse(responseCode = "400", description = "Bad Request", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Bad Request"))
+            }),
+            @APIResponse(responseCode = "404", description = "Cliente não encontrado", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Cliente não encontrado"))
+            }),
+            @APIResponse(responseCode = "409", description = "Conflito", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Conflito"))
+            })
+    })
     @POST
-    public Response criar(PedidoCreateDto pedido) {
+    public Response criar(
+            @RequestBody(description = "Dados do pedido para criação", required = true, content = @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoCreateDto.class))) PedidoCreateDto pedido) {
         Pedido pedidoEntity = pedidoMapper.toDomain(pedido);
         Pagamento pedidoPagamento = pedidoUseCase.criar(pedidoEntity);
         return Response.status(Response.Status.CREATED).entity(pedidoPagamento).build();
     }
 
+    @Operation(summary = "Buscar pedidos", description = "Busca pedidos por status")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoDto.class)) }),
+            @APIResponse(responseCode = "404", description = "Pedidos não encontrados", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedidos não encontrados"))
+            })
+    })
     @GET
-    public Response buscar(@QueryParam("status") Status status) {        
+    public Response buscar(
+            @QueryParam("status") @Parameter(description = "Status do pedido", example = "PENDENTE") Status status) {
         List<Pedido> pedidos = new ArrayList<>();
 
         if (status == null) {
@@ -64,6 +96,14 @@ public class PedidoResource {
         return Response.status(Response.Status.OK).entity(pedidosDto).build();
     }
 
+    @Operation(summary = "Buscar pedidos em andamento", description = "Busca pedidos que estão em andamento")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoDto.class)) }),
+            @APIResponse(responseCode = "404", description = "Pedidos não encontrados", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedidos não encontrados"))
+            })
+    })
     @GET
     @Path("/em-andamento")
     public Response buscarEmAndamento() {
@@ -74,9 +114,18 @@ public class PedidoResource {
         return Response.status(Response.Status.OK).entity(pedidosDto).build();
     }
 
+    @Operation(summary = "Buscar pedido por ID", description = "Busca um pedido pelo ID")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoDto.class)) }),
+            @APIResponse(responseCode = "404", description = "Pedido não encontrado", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedido não encontrado"))
+            })
+    })
     @GET
     @Path("/{id}")
-    public Response buscarPorId(@PathParam("id") UUID id) {
+    public Response buscarPorId(
+            @PathParam("id") @Parameter(description = "ID do pedido", example = "63a59178-39f8-4a28-a2c7-989a57ca7b54") UUID id) {
         if (id == null) {
             throw new ParamException(ErrosSistemaEnum.PARAM_INVALID.getMessage());
         }
@@ -86,9 +135,19 @@ public class PedidoResource {
         return Response.status(Response.Status.OK).entity(pedidoDto).build();
     }
 
+    @Operation(summary = "Atualizar pedido", description = "Atualiza um pedido existente")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoDto.class)) }),
+            @APIResponse(responseCode = "404", description = "Pedido não encontrado", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedido não encontrado"))
+            })
+    })
     @PUT
     @Path("/{id}")
-    public Response editar(@PathParam("id") UUID id, PedidoUpdateDto pedido) {
+    public Response editar(
+            @PathParam("id") @Parameter(description = "ID do pedido", example = "63a59178-39f8-4a28-a2c7-989a57ca7b54") UUID id,
+            @RequestBody(description = "Dados do pedido para atualização", required = true, content = @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoUpdateDto.class))) PedidoUpdateDto pedido) {
         if (id == null) {
             throw new ParamException(ErrosSistemaEnum.PARAM_INVALID.getMessage());
         }
@@ -101,9 +160,18 @@ public class PedidoResource {
         return Response.status(Response.Status.OK).build();
     }
 
+    @Operation(summary = "Buscar histórico de status do pedido", description = "Busca o histórico de status de um pedido pelo ID")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PedidoStatus.class)) }),
+            @APIResponse(responseCode = "404", description = "Pedido não encontrado", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedido não encontrado"))
+            })
+    })
     @GET
     @Path("/{id}/status")
-    public Response buscarHistoricoStatusPorId(@PathParam("id") UUID id) {
+    public Response buscarHistoricoStatusPorId(
+            @PathParam("id") @Parameter(description = "ID do pedido", example = "63a59178-39f8-4a28-a2c7-989a57ca7b54") UUID id) {
         if (id == null) {
             throw new ParamException(ErrosSistemaEnum.PARAM_INVALID.getMessage());
         }
@@ -113,15 +181,24 @@ public class PedidoResource {
         return Response.status(Response.Status.OK).entity(pedidosStatus).build();
     }
 
+    @Operation(summary = "Verificar se o pedido está pago", description = "Verifica se um pedido está pago pelo ID")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.BOOLEAN, example = "true")) }),
+            @APIResponse(responseCode = "404", description = "Pedido não encontrado", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(type = SchemaType.STRING, example = "Pedido não encontrado"))
+            })
+    })
     @GET
     @Path("/{id}/esta-pago")
-    public Response buscarEstaPago(@PathParam("id") UUID id) {
+    public Response buscarEstaPago(
+            @PathParam("id") @Parameter(description = "ID do pedido", example = "63a59178-39f8-4a28-a2c7-989a57ca7b54") UUID id) {
         if (id == null) {
             throw new ParamException(ErrosSistemaEnum.PARAM_INVALID.getMessage());
         }
 
         boolean pedidoEstaPago = pedidoUseCase.estaPago(id);
-        
+
         return Response.status(Response.Status.OK).entity(pedidoEstaPago).build();
     }
 
